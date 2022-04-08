fb386b1e148eef5865f96ecef984ed85ab96e295
Pedro Galvao <pedro.antunesgalvao@gmail.com>
2022-04-08 16:57:40 -0300

    excluindo arquivos não pertinentes ao front-end

--- Alunos/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from Alunos import models
-
-
-class AlunosSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Alunos
-        fields = '__all__'
--- Alunos/api/viewsets.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from Alunos.api import serializers
-from Alunos import models
-
-
-class AlunosViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.AlunosSerializer
-    queryset = models.Alunos.objects.all()
